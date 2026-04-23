bbdec0100043073be95d254f9dd24e0c1963271c
Sumanth <111695510+Sumanth386@users.noreply.github.com>
2026-04-23 14:39:01 +0530

    Updated Variables program and removed module-info

--- src/basics/Variables.java
+++ src/basics/Variables.java
@@ -3,8 +3,15 @@ package basics;
 public class Variables {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int age = 25;
+		double salary = 80000.50;
+		char grade = 'A';
+		boolean isEmployed = true;
+		
+		System.out.println("Age: " + age );
+		System.out.println("Salary: " + salary);
+		System.out.println("Grade: " + grade);
+		System.out.println("Employed: " + isEmployed);
 	}
 
 }
