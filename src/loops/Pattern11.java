2cc6d9775d8163754e4649e8433164727c9c931d
Sumanth <111695510+Sumanth386@users.noreply.github.com>
2026-04-26 21:34:58 +0530

    Improved pattern programs: fixed logic and formatting

--- src/loops/Pattern11.java
+++ src/loops/Pattern11.java
@@ -4,17 +4,14 @@ package loops;
 public class Pattern11 {
 
 	public static void main(String[] args) {
-		int n = 4;
+		int n = 5;
 		
-		for(int i = 1; i <= n; i++) {
-			// space
-			for(int j = 1; j <= i - 1; j++) {
-				System.out.print("  ");
+		for(int i = n; i >= 1; i--) {
+			for(int j = 1; j <= n - i ; j++) {
+				System.out.print(" ");
 			}
-			
-			// stars
-			for(int j = 1; j <= (2 * (n - i) + 1); j++) {
-				System.out.print("* ");
+			for(int j = 1; j <= (2 * i - 1); j++) {
+				System.out.print("*");
 			}
 			System.out.println();
 		}
